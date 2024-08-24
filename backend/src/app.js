8441064372749c2897b8170b98efc0ed9bb81f9a
shivam <rajshivam0432@gmail.com>
2024-08-25 00:44:09 +0530

    commit changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,22 +12,11 @@ import accountroutes from './routes/account.routes.js';
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 // CORS configuration
 const allowedOrigins = ['http://localhost:5173'];
 
-app.use(cors({
-    origin: function(origin, callback){
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-}));
+
 
 // Body parsing middleware
 app.use(express.json({ limit: "16kb" }));
