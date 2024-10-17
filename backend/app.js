eae0428e15bb724996d7f9b8c7802ec32803c0af
shivam <rajshivam0432@gmail.com>
2024-10-17 23:17:02 +0530

    fixes bugs

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import userRouter from './routes/user.routes.js';
-import userComplaint from './routes/profile.routes.js';
-import adminRouter from './routes/admin.routes.js';
-import feedbackroutes from './routes/feedback.routes.js';
-import rebateroutes from './routes/rebate.routes.js';
-import accountroutes from './routes/account.routes.js';
+import userRouter from "./src/routes/user.routes.js";
+import userComplaint from "./src/routes/profile.routes.js";
+import adminRouter from "./src/routes/admin.routes.js";
+import feedbackroutes from "./src/routes/feedback.routes.js";
+import rebateroutes from "./src/routes/rebate.routes.js";
+import accountroutes from "./src/routes/account.routes.js";
 
 dotenv.config();
 
@@ -16,13 +16,11 @@ const corsOptions = {
   origin: "http://localhost:5173", // Replace with your specific frontend URL
   credentials: true, // This allows the server to accept credentials (cookies, authorization headers, etc.)
 };
-  
-  app.use(cors(corsOptions));
-  app.options('*', cors(corsOptions)); 
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 // CORS configuration
 // const allowedOrigins = ['http://localhost:5173'];
-
-
 
 // Body parsing middleware
 app.use(express.json({ limit: "16kb" }));
@@ -34,25 +32,25 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // Root route
-app.get('/', (req, res) => {
-    res.send("Server is ready to use!!!");
+app.get("/", (req, res) => {
+  res.send("Server is ready to use!!!");
 });
 
 // API routes
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/admin', adminRouter);
-app.use('/api/v1/profile', userComplaint);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/profile", userComplaint);
 app.use("/feedback", feedbackroutes);
 app.use("/api/v1/rebate/post", rebateroutes);
 app.use("/api/v1/account", accountroutes);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
-    res.status(statusCode).json({ success: false, statusCode, message });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ success: false, statusCode, message });
 });
 
 // Handle CORS preflight requests
-app.options('*', cors());
+app.options("*", cors());
 
 export default app;
